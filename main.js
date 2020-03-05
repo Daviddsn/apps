b06c16cceacae1462d527c3b9221ab66a39518a9
Daviddsn <davisdn@dcomp.ufs.br>
2020-03-05 16:23:00 -0300

    app-finish

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const binValue = document.querySelector('main input#bin');
 const resultValue = document.querySelector('main input#resultado');
 
+function resolve (){
+    if(binValue.value.match(/^[0-1]+$/g) === null){
+        document.querySelector('label').style.display ="inline";
+        resultValue.value="";        
+    }else{
+        document.querySelector('label').style.display ="none";
+        const valor=binValue.value;
+        resultValue.value = parseInt(valor,2);
+       }
+    };
 
 
-function renderResult(){
- const valor=binValue.value;
- resultValue.value = parseInt(valor,2);
- 
-}
